9ed9426c56d2776eb244cd343f00bba9597e46dd
Platon Lukyanov <platonioluka@gmail.com>
2024-10-30 15:33:17 +0300

    Added matrix representation

--- bmpHeader.h
+++ bmpHeader.h
@@ -1,32 +1,32 @@
 #ifndef BITMAP_HEADER
 #define BITMAP_HEADER
-#include <iostream>
-#include <fstream>
 #include <cstdint>
+#include <fstream>
+#include <iostream>
 
 // bf=bitmap file
 // bi=bitmap info
-#pragma pack(push, 1) 
+#pragma pack(push, 1)
 struct BITMAPFILEHEADER {
-    uint16_t bfType;    
-    uint32_t bfSize;   
-    uint16_t bfReserved1;
-    uint16_t bfReserved2;
-    uint32_t bfOffsetBits;
+        uint16_t bfType;
+        uint32_t bfSize;
+        uint16_t bfReserved1;
+        uint16_t bfReserved2;
+        uint32_t bfOffsetBits;
 };
 
 struct BITMAPINFOHEADER {
-    uint32_t biSize; 
-    int32_t biWidth;
-    int32_t biHeight;
-    uint16_t biNumberOfColorPlanes;
-    uint16_t biBitPerPixel;
-    uint32_t biCompressionType;   //  (0 = none)
-    uint32_t biImageDataSize;
-    int32_t biXPixelsPerMeter;
-    int32_t biYPixelsPerMeter;
-    uint32_t biNumberOfColorsUsed;
-    uint32_t biImportantColors;
+        uint32_t biSize;
+        int32_t biWidth;
+        int32_t biHeight;
+        uint16_t biNumberOfColorPlanes;
+        uint16_t biBitPerPixel;
+        uint32_t biCompressionType;  //  (0 = none)
+        uint32_t biImageDataSize;
+        int32_t biXPixelsPerMeter;
+        int32_t biYPixelsPerMeter;
+        uint32_t biNumberOfColorsUsed;
+        uint32_t biImportantColors;
 };
 #pragma pack(pop)
 #endif
--- main.cpp
+++ main.cpp
@@ -1,56 +1,126 @@
-#include <iostream>
 #include <fstream>
+#include <iostream>
+
 #include "bmpHeader.h"
+
+struct Pixel {
+        uint8_t blue;
+        uint8_t green;
+        uint8_t red;
+};
+
+class BMPPixelMatrix {
+    public:
+        BMPPixelMatrix(uint8_t* rawPixels, int width, int height,
+                       int bytesPerPixel)
+            : _width(width), _height(height) {
+            loadPixelMatrix(rawPixels, bytesPerPixel);
+        }
+        ~BMPPixelMatrix() {
+            delete[] matrix;
+        }
+
+        uint8_t* convertMatrixToRawPixels() {
+            int bytesPerPixel = (bytesPerPixel / 8);
+            int rowSize = ((bytesPerPixel * _width + 3) &
+                           ~3);  // Round up to nearest multiple of 4
+
+            uint8_t* rawPixels = new uint8_t[_height * rowSize];
+
+            for (int y = 0; y < _height; ++y) {
+                for (int x = 0; x < _width; ++x) {
+                    int index = (y * rowSize + x * bytesPerPixel);
+                    rawPixels[index] = matrix[_height - 1 - y][x].blue;
+                    rawPixels[index + 1] = matrix[_height - 1 - y][x].green;
+                    rawPixels[index + 2] = matrix[_height - 1 - y][x].red;
+                }
+                // Add padding if necessary
+                for (int p = bytesPerPixel * _width; p < rowSize; ++p) {
+                    rawPixels[y * rowSize + p] =
+                        0;  // Padding bytes set to zero
+                }
+            }
+
+            return rawPixels;
+        }
+
+    private:
+        int _width;
+        int _height;
+        Pixel** matrix;
+        void loadPixelMatrix(uint8_t* rawPixels, int bitPerPixel) {
+            Pixel** matrix = new Pixel*[_height];
+            for (int i = 0; i < _height; ++i) {
+                matrix[i] = new Pixel[_width];
+            }
+
+            int bytesPerPixel = (bitPerPixel / 8);
+            int rowSize = ((bytesPerPixel * _width + 3) &
+                           ~3);  // Round up to nearest multiple of 4
+
+            for (int y = 0; y < _height; ++y) {
+                for (int x = 0; x < _width; ++x) {
+                    int index =
+                        ((_height - 1 - y) * rowSize + x * bytesPerPixel);
+                    matrix[y][x].blue = rawPixels[index];
+                    matrix[y][x].green = rawPixels[index + 1];
+                    matrix[y][x].red = rawPixels[index + 2];
+                }
+            }
+        }
+};
 
 class BMPFile {
     public:
-        BMPFile(const char* filename) : _filename(filename) {
-            load();
-        }
+        BMPFile(const char* filename) : _filename(filename) { load(); }
         ~BMPFile() {
             delete[] pixels;
+            delete pixelMatrix;
         }
-        int getWidth() {
-            return _infoHeader.biWidth;
-        }
-        int getHeight() {
-            return _infoHeader.biHeight;
-        }
+        int getWidth() { return _infoHeader.biWidth; }
+        int getHeight() { return _infoHeader.biHeight; }
         void write(const char* newFilename) {
             std::ofstream file(newFilename, std::ios::binary);
- 
             int height = getHeight();
             int width = getWidth();
 
             BITMAPFILEHEADER fileHeader = _fileHeader;
             BITMAPINFOHEADER infoHeader = _infoHeader;
-            
+
             if (!file) {
                 std::cerr << "Unable to open file!" << std::endl;
                 return;
             }
-            file.write(reinterpret_cast<const char*>(&fileHeader), sizeof(fileHeader));
-            file.write(reinterpret_cast<const char*>(&infoHeader), sizeof(infoHeader));
+            file.write(reinterpret_cast<const char*>(&fileHeader),
+                       sizeof(fileHeader));
+            file.write(reinterpret_cast<const char*>(&infoHeader),
+                       sizeof(infoHeader));
             file.write(reinterpret_cast<const char*>(pixels), getImageSize());
             file.close();
         }
+
     private:
         uint8_t* pixels;
         const char* _filename;
         BITMAPFILEHEADER _fileHeader;
         BITMAPINFOHEADER _infoHeader;
         static const uint16_t bmInLittleEndian = 0x4D42;
- 
+        BMPPixelMatrix* pixelMatrix;
+
+        void setPixelMatrix(BMPPixelMatrix* matrix) { pixelMatrix = matrix; }
+
         void load() {
-            std::ifstream file(_filename, std::ios::binary); 
+            std::ifstream file(_filename, std::ios::binary);
 
             if (!file) {
                 std::cerr << "Unable to open file!" << std::endl;
                 return;
             }
 
-            file.read(reinterpret_cast<char*>(&_fileHeader), sizeof(_fileHeader));
-            file.read(reinterpret_cast<char*>(&_infoHeader), sizeof(_infoHeader));
+            file.read(reinterpret_cast<char*>(&_fileHeader),
+                      sizeof(_fileHeader));
+            file.read(reinterpret_cast<char*>(&_infoHeader),
+                      sizeof(_infoHeader));
 
             if (_fileHeader.bfType != bmInLittleEndian) {
                 std::cerr << "Not a BMP file!" << std::endl;
@@ -62,13 +132,16 @@ class BMPFile {
             pixels = new uint8_t[imageSize];
 
             file.read(reinterpret_cast<char*>(pixels), imageSize);
+            setPixelMatrix(new BMPPixelMatrix(pixels, getWidth(), getHeight(),
+                                              _infoHeader.biBitPerPixel));
             file.close();
         }
 
         int getImageSize() {
             size_t imageSize = _infoHeader.biImageDataSize;
             if (imageSize == 0) {
-                imageSize = _infoHeader.biWidth * _infoHeader.biHeight * (_infoHeader.biBitPerPixel / 8);
+                imageSize = _infoHeader.biWidth * _infoHeader.biHeight *
+                            (_infoHeader.biBitPerPixel / 8);
             }
             return imageSize;
         }
@@ -76,7 +149,7 @@ class BMPFile {
 
 int main() {
     std::cout << "Hello, world!\n";
-    BMPFile bmpFile("sample.bmp");
-    std::cout << bmpFile.getWidth() << " and " << bmpFile.getHeight();
-    bmpFile.write("sample2.bmp");
+    BMPFile bmpFile("example.bmp");
+    std::cout << bmpFile.getWidth() << " and " << bmpFile.getHeight() << "\n";
+    bmpFile.write("example3.bmp");
 }
